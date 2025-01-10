c2273d65fb8305804d21f68fa13b6d3a72d58af2
AleksandrPy <santiagogysakov@gmail.com>
2025-01-10 14:19:55 +0300

    Добавил задание №3

--- cyberchase/3.sql
+++ cyberchase/3.sql
@@ -1,2 +1,4 @@
 -- Найтри production code для эпизода "Hackerized!"
-
+SELECT `production code` FROM `episodes`
+WHERE `title` = '"Hackerized!"';
+-- Как нужно правильно написать '"Hackerized!"'? Выдаёт ошибку
