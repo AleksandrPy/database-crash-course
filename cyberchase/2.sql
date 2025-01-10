ae71f6d438117499d7e59ef804b78071d1bd8a12
AleksandrPy <santiagogysakov@gmail.com>
2025-01-10 14:00:50 +0300

    Добавил задание №2

--- cyberchase/2.sql
+++ cyberchase/2.sql
@@ -1,2 +1,5 @@
 -- Вывести номер сезона и заголовок первых эпизодов каждого сезона
-
+SELECT `season`,
+MIN(episode_in_season) AS `first_episode_number`, `title`
+FROM `episodes`
+GROUP BY `season`;
